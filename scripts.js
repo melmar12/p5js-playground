928ea5d8c1e9aa895e57e1be85f6fe4df0865624
Melissa Martinez <melmar12@live.com>
2017-06-05 11:29:46 -0700

    made func reusable

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
 window.onload = function myFunction() {
+
+
+
+    function insertiFrame(url, parent, sibling) {
+      var x = document.createElement("IFRAME");
+      x.setAttribute("src", url);
+      x.style.display = "block"; // removes gap
+      x.style.zIndex = "-1";
+      x.style.width = "100%";
+      var sibHeight = document.getElementById(sibling).clientHeight.toString();
+      x.style.height = sibHeight + "px";
+      x.style.marginTop = "-" + sibHeight + "px";
+      x.style.border = "none";
+      document.getElementById(parent).appendChild(x);
+    }
+
     //insert star field
-    var x = document.createElement("IFRAME");
-    x.setAttribute("src", "stars/stars.html");
-    x.style.display = "block"; // removes gap
-    x.style.zIndex = "-1";
-    x.style.width = "100%";
-    var elmHeight = document.getElementById('module').clientHeight.toString();
-    x.style.height = elmHeight + "px";
-    x.style.marginTop = "-" + elmHeight + "px";
-    x.style.border = "none";
-    document.getElementById('star-field').appendChild(x);
+    insertiFrame('stars/stars.html', 'star-field', 'module');
+    insertiFrame('stars/stars.html', 'second-star-field', 'third-module');
+
 }
